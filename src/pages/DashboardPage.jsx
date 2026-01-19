27942ea67228dedab28ea1da3213522f374c7545
vmelniki2828 <76560283+vmelniki2828@users.noreply.github.com>
2026-01-19 03:06:47 +0200

    asd

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
 import { Layout } from '../components/Layout';
 import { useTheme } from '../contexts/ThemeContext';
 import { HiArrowUp, HiArrowDown } from 'react-icons/hi2';
